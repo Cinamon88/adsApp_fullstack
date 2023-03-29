a1af898b97b93f04b0cede39e29c041184a71928
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-29 23:03:58 +0200

    modify

--- db.js
+++ db.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 
 const connectToDB = () => {
-
+  // connect to DB
   const NODE_ENV = process.env.NODE_ENV;
-    console.log('Node', NODE_ENV);
-    if (NODE_ENV === "production")
-    dbatlas = `mongodb+srv://Cinamon88:zkPINxtP8tKJyTJl@adsdb.qxhjfgi.mongodb.net/adsDB?retryWrites=true&w=majority`;
-    else if (NODE_ENV === "test") dbatlas = "mongodb://localhost:27017/adsDBtest";
-    else dbatlas = "mongodb://localhost:27017/adsDB";
-  
-    // connects our backend code with the database
-    mongoose.connect(dbatlas, {
-        useNewUrlParser: true,
-    });
-    const db = mongoose.connection;
-    
-    db.once("open", () => {
-        console.log("Connected to the database");
-    });
-    
-    db.on("error", (err) => console.log("Error " + err));
+  let dbUri = '';
+
+  if (NODE_ENV === 'production') dbUri = process.env.mongo_url;
+  else if (NODE_ENV === 'test')
+    dbUri = 'mongodb://localhost:27017/adsDBtest';
+  else dbUri = 'mongodb://localhost:27017/adsDB';
+
+  mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = mongoose.connection;
+
+  // on success
+  db.once('open', () => {
+    console.log('Connected to the database');
+  });
+
+  // on error
+  db.on('error', (err) => console.log('Error ' + err));
 };
 
 module.exports = connectToDB;
